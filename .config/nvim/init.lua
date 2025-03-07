c52b63c39053918e20383a2f6b36f470672804e7
anhtran <uiui1999vn@gmail.com>
2025-03-07 15:20:11 +0700

    update lazy for copitlot and disable gitignore for nvim-tree

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -50,6 +50,20 @@ require('lspconfig').pyright.setup {
     },
   },
 }
+-- nvim tree 
+require("nvim-tree").setup{
+     filters = {
+        enable = true,
+        git_ignored = false,
+        dotfiles = false,
+        git_clean = false,
+        no_buffer = false,
+        no_bookmark = false,
+        custom = {},
+        exclude = {},
+      },
+
+}
 
 -- load treesitter
 require'nvim-treesitter.configs'.setup {
