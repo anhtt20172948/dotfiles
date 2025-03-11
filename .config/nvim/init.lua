690751021cd22e1ff8fbed81ef23656b261ef78c
anhtran <uiui1999vn@gmail.com>
2025-03-11 11:14:24 +0700

    configure Copilot key mapping and disable default tab mapping

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -71,6 +71,12 @@ require("nvim-tree").setup {
 
 }
 
+-- copilot
+vim.keymap.set('i', '<Right>', 'copilot#Accept("\\<CR>")', {
+    expr = true,
+    replace_keycodes = false
+})
+vim.g.copilot_no_tab_map = true
 
 
 -- load treesitter
