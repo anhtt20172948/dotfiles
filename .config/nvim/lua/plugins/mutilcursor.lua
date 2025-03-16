3cda4c00694a527f1196240438bf7f9cb76a54b9
uiui1999vn <uiui1999vn@gmail.com>
2025-03-16 23:53:13 +0700

    update keybinding for multicursor

--- .config/nvim/lua/plugins/mutilcursor.lua
+++ .config/nvim/lua/plugins/mutilcursor.lua
@@ -10,18 +10,20 @@ return {
             local set = vim.keymap.set
 
             -- Add or skip cursor above/below the main cursor.
-            set({ "n", "x" }, "<up>", function() mc.lineAddCursor(-1) end)
-            set({ "n", "x" }, "<down>", function() mc.lineAddCursor(1) end)
-            set({ "n", "x" }, "<leader><up>", function() mc.lineSkipCursor(-1) end)
-            set({ "n", "x" }, "<leader><down>", function() mc.lineSkipCursor(1) end)
+            set({ "n", "x" }, "<up>", function() mc.lineAddCursor(-1) end, { desc = "Add cursor above" })
+            set({ "n", "x" }, "<down>", function() mc.lineAddCursor(1) end, { desc = "Add cursor below" })
+            set({ "n", "x" }, "<leader><up>", function() mc.lineSkipCursor(-1) end, { desc = "Skip cursor above" })
+            set({ "n", "x" }, "<leader><down>", function() mc.lineSkipCursor(1) end, { desc = "Skip cursor below" })
 
             -- Add or skip adding a new cursor by matching word/selection
-            set({ "n", "x" }, "<leader>n", function() mc.matchAddCursor(1) end)
-            set({ "n", "x" }, "<leader>s", function() mc.matchSkipCursor(1) end)
-            set({ "n", "x" }, "<leader>N", function() mc.matchAddCursor(-1) end)
-            set({ "n", "x" }, "<leader>S", function() mc.matchSkipCursor(-1) end)
+            set({ "n", "x" }, "<leader>/n", function() mc.matchAddCursor(1) end, { desc = "add cursor at next match" })
+            set({ "n", "x" }, "<leader>/s", function() mc.matchskipcursor(1) end, { desc = "Skip cursor at next match" })
+            set({ "n", "x" }, "<leader>/N", function() mc.matchAddCursor(-1) end,
+                { desc = "Add cursor at previous match" })
+            set({ "n", "x" }, "<leader>/S", function() mc.matchSkipCursor(-1) end,
+                { desc = "Skip cursor at previous match" })
             -- Add all matches in the document
-            set({ "n", "v" }, "<leader>A", mc.matchAllAddCursors)
+            set({ "n", "v" }, "<leader>A", mc.matchAllAddCursors, { desc = "Add cursors all matches" })
 
             -- Add and remove cursors with control + left click.
             set("n", "<c-leftmouse>", mc.handleMouse)
@@ -35,18 +37,18 @@ return {
             -- multiple cursors. This lets you have overlapping mappings.
             mc.addKeymapLayer(function(layerSet)
                 -- Select a different cursor as the main one.
-                layerSet({ "n", "x" }, "<left>", mc.prevCursor)
-                layerSet({ "n", "x" }, "<right>", mc.nextCursor)
+                layerSet({ "n", "x" }, "<left>", mc.prevCursor, { desc = "Previous cursor" })
+                layerSet({ "n", "x" }, "<right>", mc.nextCursor, { desc = "Next cursor" })
 
                 -- Delete the main cursor.
-                layerSet({ "n", "x" }, "<leader>x", mc.deleteCursor)
+                layerSet({ "n", "x" }, "<leader>x", mc.deleteCursor, { desc = "Delete cursor" })
 
                 -- Enable and clear cursors using escape.
-                layerSet("n", "<esc>", function()
-                    if not mc.cursorsEnabled() then
-                        mc.enableCursors()
+                layerset("n", "<esc>", function()
+                    if not mc.cursorsenabled() then
+                        mc.enablecursors()
                     else
-                        mc.clearCursors()
+                        mc.clearcursors()
                     end
                 end)
             end)
