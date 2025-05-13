bed74adc76e324340a0a646236a7de71ba98ffbb
anhtran <uiui1999vn@gmail.com>
2025-05-13 09:53:59 +0700

    [update] remove unused keymaps for selecting marked files in harpoon2 plugin

--- .config/nvim/lua/plugins/harpoon2.lua
+++ .config/nvim/lua/plugins/harpoon2.lua
@@ -36,20 +36,6 @@ return {
 			harpoon.ui:toggle_quick_menu(harpoon:list())
 		end)
 
-		--Harpoon marked files
-		vim.keymap.set("n", "<C-y>", function()
-			harpoon:list():select(1)
-		end)
-		vim.keymap.set("n", "<C-i>", function()
-			harpoon:list():select(2)
-		end)
-		vim.keymap.set("n", "<C-n>", function()
-			harpoon:list():select(3)
-		end)
-		vim.keymap.set("n", "<C-s>", function()
-			harpoon:list():select(4)
-		end)
-
 		-- Toggle previous & next buffers stored within Harpoon list
 		vim.keymap.set("n", "<C-S-P>", function()
 			harpoon:list():prev()
