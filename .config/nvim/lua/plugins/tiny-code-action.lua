9fc3803e68aed71a0e80fca0506d146eb446f654
anhtran <uiui1999vn@gmail.com>
2025-05-13 14:57:03 +0700

    [update] enhance tiny-code-action configuration with custom picker options

--- .config/nvim/lua/plugins/tiny-code-action.lua
+++ .config/nvim/lua/plugins/tiny-code-action.lua
@@ -2,12 +2,6 @@ return {
     "rachartier/tiny-code-action.nvim",
     dependencies = {
         {"nvim-lua/plenary.nvim"},
-
-        -- optional picker via telescope
-        {"nvim-telescope/telescope.nvim"},
-        -- optional picker via fzf-lua
-        {"ibhagwan/fzf-lua"},
-        -- .. or via snacks
         {
           "folke/snacks.nvim",
           opts = {
@@ -16,5 +10,21 @@ return {
         }
     },
     event = "LspAttach",
-    opts = {},
+    opts = {
+        picker = {
+        "snacks",
+        opts = {
+          layout_strategy = "vertical",
+          layout_config = {
+            width = 0.7,
+            height = 0.9,
+            preview_cutoff = 1,
+            preview_height = function(_, _, max_lines)
+              local h = math.floor(max_lines * 0.5)
+              return math.max(h, 10)
+            end,
+          },
+        },
+      },
+    },
 }
