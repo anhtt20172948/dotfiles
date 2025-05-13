f293ab5389ef3fbfaa2fae0d61c92dd0bcc96233
anhtran <uiui1999vn@gmail.com>
2025-05-13 09:20:22 +0700

    [add] implement harpoon2 plugin with keymaps for navigation

---
+++ .config/nvim/lua/plugins/harpoon2.lua
@@ -0,0 +1,88 @@
+
+return {
+	"thePrimeagen/harpoon",
+	branch = "harpoon2",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-telescope/telescope.nvim",
+	},
+	config = function()
+		local harpoon = require("harpoon")
+		local conf = require("telescope.config").values
+
+		harpoon:setup({
+			global_settings = {
+				save_on_toggle = true,
+				save_on_change = true,
+			},
+		})
+
+        -- picker
+        local function generate_harpoon_picker()
+            local file_paths = {}
+            for _, item in ipairs(harpoon:list().items) do
+                table.insert(file_paths, {
+                    text = item.value,
+                    file = item.value
+                })
+            end
+            return file_paths
+        end
+
+		--Harpoon Nav Interface
+		vim.keymap.set("n", "<leader>a", function()
+			harpoon:list():add()
+		end, { desc = "Harpoon add file" })
+		vim.keymap.set("n", "<C-e>", function()
+			harpoon.ui:toggle_quick_menu(harpoon:list())
+		end)
+
+		--Harpoon marked files
+		vim.keymap.set("n", "<C-y>", function()
+			harpoon:list():select(1)
+		end)
+		vim.keymap.set("n", "<C-i>", function()
+			harpoon:list():select(2)
+		end)
+		vim.keymap.set("n", "<C-n>", function()
+			harpoon:list():select(3)
+		end)
+		vim.keymap.set("n", "<C-s>", function()
+			harpoon:list():select(4)
+		end)
+
+		-- Toggle previous & next buffers stored within Harpoon list
+		vim.keymap.set("n", "<C-S-P>", function()
+			harpoon:list():prev()
+		end)
+		vim.keymap.set("n", "<C-S-N>", function()
+			harpoon:list():next()
+		end)
+
+		-- Telescope inside Harpoon Window
+        vim.keymap.set("n", "<leader>fh", function() Snacks.picker({
+        finder = generate_harpoon_picker,
+            win = {
+                input = {
+                    keys = {
+                        ["dd"] = { "harpoon_delete", mode = { "n", "x" } }
+                    }
+                },
+                list = {
+                    keys = {
+                        ["dd"] = { "harpoon_delete", mode = { "n", "x" } }
+                    }
+                },
+            },
+        actions = {
+            harpoon_delete = function(picker, item)
+                local to_remove = item or picker:selected()
+                table.remove(harpoon:list().items, to_remove.idx)
+                picker:find({
+                    refresh = true -- refresh picker after removing values
+                })
+            end
+        },
+    }) end)
+end,
+}
