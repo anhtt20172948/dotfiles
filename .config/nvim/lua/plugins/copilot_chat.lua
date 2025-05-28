187bc8b457c7adc4b77ca49e444fb26e4723266e
uiui1999vn <uiui1999vn@gmail.com>
2025-05-28 22:37:19 +0700

    [add] nvim surround , [fix] key for todo-comments [update] config copilotchat

--- .config/nvim/lua/plugins/copilot_chat.lua
+++ .config/nvim/lua/plugins/copilot_chat.lua
@@ -7,9 +7,18 @@ return {
 			{ "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
 		},
 		build = "make tiktoken", -- Only on MacOS or Linux
-		opts = {
-			-- See Configuration section for options
-		},
+		opts = function()
+			local user = vim.env.USER or "User"
+			user = user:sub(1, 1):upper() .. user:sub(2)
+			return {
+				auto_insert_mode = true,
+				question_header = "  " .. user .. " ",
+				answer_header = "  Copilot ",
+				window = {
+					width = 0.4,
+				},
+			}
+		end,
 		config = function()
 			require("CopilotChat").setup({
 
--- .config/nvim/lua/plugins/todo-comments.lua
+++ .config/nvim/lua/plugins/todo-comments.lua
@@ -69,29 +69,29 @@ return {
 			pattern = [[\b(KEYWORDS):]], -- ripgrep regex
 			-- pattern = [[\b(KEYWORDS)\b]], -- match without the extra colon. You'll likely get false positives
 		},
-		keys = {
-			{
-				"]t",
-				function()
-					require("todo-comments").jump_next()
-				end,
-				desc = "Next Todo Comment",
-			},
-			{
-				"[t",
-				function()
-					require("todo-comments").jump_prev()
-				end,
-				desc = "Previous Todo Comment",
-			},
-			{ "<leader>xt", "<cmd>Trouble todo toggle<cr>", desc = "Todo (Trouble)" },
-			{
-				"<leader>xT",
-				"<cmd>Trouble todo toggle filter = {tag = {TODO,FIX,FIXME}}<cr>",
-				desc = "Todo/Fix/Fixme (Trouble)",
-			},
-			{ "<leader>st", "<cmd>TodoTelescope<cr>", desc = "Todo" },
-			{ "<leader>sT", "<cmd>TodoTelescope keywords=TODO,FIX,FIXME<cr>", desc = "Todo/Fix/Fixme" },
+	},
+	keys = {
+		{
+			"]t",
+			function()
+				require("todo-comments").jump_next()
+			end,
+			desc = "Next Todo Comment",
 		},
+		{
+			"[t",
+			function()
+				require("todo-comments").jump_prev()
+			end,
+			desc = "Previous Todo Comment",
+		},
+		{ "<leader>xt", "<cmd>Trouble todo toggle<cr>", desc = "Todo (Trouble)" },
+		{
+			"<leader>xT",
+			"<cmd>Trouble todo toggle filter = {tag = {TODO,FIX,FIXME}}<cr>",
+			desc = "Todo/Fix/Fixme (Trouble)",
+		},
+		{ "<leader>st", "<cmd>TodoTelescope<cr>", desc = "Todo" },
+		{ "<leader>sT", "<cmd>TodoTelescope keywords=TODO,FIX,FIXME<cr>", desc = "Todo/Fix/Fixme" },
 	},
 }
