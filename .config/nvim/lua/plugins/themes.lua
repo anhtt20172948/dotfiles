0b0b97d7beb5bdd6971f4d758e79f9d06fc09cb1
anhtran <uiui1999vn@gmail.com>
2025-05-30 15:50:28 +0700

    [update] intergrated buffer line and catppuccin theme

--- .config/nvim/lua/plugins/themes.lua
+++ .config/nvim/lua/plugins/themes.lua
@@ -196,6 +196,28 @@ return {
 		end,
 	},
 	{
+		"akinsho/bufferline.nvim",
+		config = function(_, opts)
+			local mocha = require("catppuccin.palettes").get_palette("mocha")
+			require("bufferline").setup({
+				highlights = require("catppuccin.groups.integrations.bufferline").get({
+					styles = { "italic", "bold" },
+					custom = {
+						all = {
+							fill = { bg = "#000000" },
+						},
+						mocha = {
+							background = { fg = mocha.text },
+						},
+						latte = {
+							background = { fg = "#000000" },
+						},
+					},
+				}),
+			})
+		end,
+	},
+	{
 		"folke/tokyonight.nvim",
 		lazy = true,
 		priority = 1000,
