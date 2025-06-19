b740c083e9d4569c4e4f0c79e5cf8b831f200023
anhtran <anhtt172948@gmail.com>
2025-06-19 16:22:18 +0700

    [update] integrated blink with colorful menu

--- .config/nvim/lua/plugins/blink-cmp.lua
+++ .config/nvim/lua/plugins/blink-cmp.lua
@@ -234,6 +234,32 @@ return {
 						{ "kind_icon", "label", gap = 1 },
 						{ "kind" },
 					},
+					components = {
+						label = {
+							width = { fill = true, max = 60 },
+							text = function(ctx)
+								local highlights_info = require("colorful-menu").blink_highlights(ctx)
+								if highlights_info ~= nil then
+									-- Or you want to add more item to label
+									return highlights_info.label
+								else
+									return ctx.label
+								end
+							end,
+							highlight = function(ctx)
+								local highlights = {}
+								local highlights_info = require("colorful-menu").blink_highlights(ctx)
+								if highlights_info ~= nil then
+									highlights = highlights_info.highlights
+								end
+								for _, idx in ipairs(ctx.label_matched_indices) do
+									table.insert(highlights, { idx, idx + 1, group = "BlinkCmpLabelMatch" })
+								end
+								-- Do something else
+								return highlights
+							end,
+						},
+					},
 				},
 			},
 			documentation = {
